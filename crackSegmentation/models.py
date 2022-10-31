2542534b2a0c16383df31331fc91b88c7694f37f
Jeonjunhowi <jjh63360@kau.kr>
2022-10-31 10:14:19 +0900

    add: resize image to 448x448 and save

--- crackSegmentation/models.py
+++ crackSegmentation/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class FileUpload(models.Model):
-    title = models.TextField(max_length=40, null=True)
+    title = models.TextField(null=True)
     imgfile = models.ImageField(null=True, upload_to="", blank=True)
     content = models.TextField()
 
--- crackSegmentation/views.py
+++ crackSegmentation/views.py
@@ -6,17 +6,30 @@ from django.shortcuts import render, redirect
 from .forms import FileUploadForm
 from .models import FileUpload
 from django.views.decorators.csrf import csrf_exempt
+from PIL import Image
+import cv2
+import os
 
 @csrf_exempt
 def fileUpload(request):
     if request.method == 'POST':
         title = request.POST['title']
         img = request.FILES["imgfile"]
+
         fileupload = FileUpload(
             title=title,
             imgfile=img,
         )
         fileupload.save()
+        print(str(img))
+        print(os.path.isdir('media/images/' + str(img)))
+        print(os.path.isdir('media/images/KakaoTalk_Photo_2022-08-23-23-07-58_002.jpeg'))
+
+
+        resized_img = cv2.imread('media/images/KakaoTalk_Photo_2022-08-23-23-07-58_002.jpeg')
+        resized_img = cv2.resize(resized_img, (448, 448))
+        cv2.imwrite('media/resized/' +'resized_'+ str(img), resized_img)
+        # os.system("명령어")
         return HttpResponse(str(img)+" file-Uploaded")
     else:
         fileuploadForm = FileUploadForm
@@ -28,4 +41,21 @@ def fileUpload(request):
 
 def testResponse(request):
     return HttpResponse("Hello world!")
+
+
+def rescale(image, width):
+    img = Image.open(image)
+
+    src_width, src_height = img.size
+    src_ratio = float(src_height) / float(src_width)
+    dst_height = round(src_ratio * width)
+
+    img = img.resize((width, dst_height), Image.LANCZOS)
+    img.save(image.name, 'JPEG')
+    image.file = img
+
+    # 이게 없으면 attribute error 발생
+    image.file.name = image.name
+
+    return image
 
