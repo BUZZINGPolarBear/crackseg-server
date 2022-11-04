c04df1b9eb7130ffd46cc6389e2ff4bf219ed225
Jeonjunhowi <jjh63360@kau.kr>
2022-11-04 09:57:12 +0900

    fix: loading page style

---
+++ templates/static/js/index-second.js
@@ -0,0 +1,3 @@
+window.onload = function(){
+  console.log(localStorage.getItem("pic_name"))
+}
--- templates/static/js/index.js
+++ templates/static/js/index.js
@@ -5,12 +5,14 @@ imgInp.onchange = evt => {
   const [file] = imgInp.files
   if (file) {
     crack_img.src = URL.createObjectURL(file)
+    localStorage.setItem("pic_url", toString(crack_img.src))
   }
 }
 
 $("#imgInp").on('change',function(){
   fileName = $("#imgInp").val();
   $(".upload-name").val(fileName);
+  localStorage.setItem("pic_name", fileName)
 });
 
 function toLoadingPage(){
