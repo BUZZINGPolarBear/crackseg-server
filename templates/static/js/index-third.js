a39ed3b29735a8d759bf25c13fceb5bcfd14e06f
Jeonjunhowi <jjh63360@kau.kr>
2022-11-10 12:20:19 +0900

    add: homepage locater

--- templates/static/js/index-third.js
+++ templates/static/js/index-third.js
@@ -2,3 +2,7 @@ window.onload = function(){
   const location = "/templates/static/images/predicted/resized_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
   predicted_img.src = location
 }
+
+function toHome(){
+  location.href="/";
+}
