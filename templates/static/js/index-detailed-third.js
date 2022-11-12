94e13a069f2f3d1e5d54ee6be762e0a554e8f523
Jeonjunhowi <jjh63360@kau.kr>
2022-11-12 18:50:23 +0900

    add: 상단, 중단, 하단의 균열 검출 결과 분할&전시

--- templates/static/js/index-detailed-third.js
+++ templates/static/js/index-detailed-third.js
@@ -1,16 +1,35 @@
-window.onload = function(){
-  const location = "/templates/static/images/predicted/resized_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
-  const resized_img_location = "/templates/static/images/resized/resized_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
-  predicted_img.src = location
-  resized_img.src = resized_img_location
-}
+// window.onload = function(){
+//   const location = "/templates/static/images/predicted/resized_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
+//   const resized_img_location = "/templates/static/images/resized/resized_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
+//   predicted_img.src = location
+//   resized_img.src = resized_img_location
+// }
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("value");
-output.innerHTML = slider.value; // Display the default slider value
+var topSlider = document.getElementById("topImgSlider");
+var topOutput = document.getElementById("topValue");
+topOutput.innerHTML = topSlider.value; // Display the default slider value
+
+var midSlider = document.getElementById("midImgSlider");
+var midOutput = document.getElementById("midValue");
+midOutput.innerHTML = midSlider.value; // Display the default slider value
+
+var botSlider = document.getElementById("botImgSlider");
+var botOutput = document.getElementById("botValue");
+botOutput.innerHTML = botSlider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-  $('#predicted_img').css('opacity', (this.value/100));
+topSlider.oninput = function() {
+  topOutput.innerHTML = this.value;
+  $('.topRow').css('opacity', (this.value/100));
 }
+
+midSlider.oninput = function() {
+  midOutput.innerHTML = this.value;
+  $('.midRow').css('opacity', (this.value/100));
+}
+
+botSlider.oninput = function() {
+  botOutput.innerHTML = this.value;
+  $('.botRow').css('opacity', (this.value/100));
+}
+
