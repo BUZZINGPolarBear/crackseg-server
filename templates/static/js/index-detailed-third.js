c088dbe6cc7f6ac674582b80614600dee26971d4
Jeonjunhowi <jjh63360@kau.kr>
2022-11-12 19:21:26 +0900

    add: 정밀 이미지 종적 전시 완료

--- templates/static/js/index-detailed-third.js
+++ templates/static/js/index-detailed-third.js
@@ -5,6 +5,40 @@
 //   resized_img.src = resized_img_location
 // }
 
+window.onload = function(){
+  //좌상단
+  leftTopResized.src = "/templates/static/images/resized/resized_leftTop_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
+  leftTopPredicted.src = "/templates/static/images/predicted/resized_leftTop_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
+  //중상단
+  midTopResized.src = "/templates/static/images/resized/resized_midTop_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
+  midTopPredicted.src = "/templates/static/images/predicted/resized_midTop_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
+  //우상단
+  rightTopResized.src = "/templates/static/images/resized/resized_rightTop_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
+  rightTopPredicted.src = "/templates/static/images/predicted/resized_rightTop_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
+
+  //----- 중간 -----
+  //좌중단
+  leftMidResized.src = "/templates/static/images/resized/resized_leftMid_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
+  leftMidPredicted.src = "/templates/static/images/predicted/resized_leftMid_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
+  //중중단
+  midMidResized.src = "/templates/static/images/resized/resized_midMid_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
+  midMidPredicted.src = "/templates/static/images/predicted/resized_midMid_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
+  //우중단
+  rightMidResized.src = "/templates/static/images/resized/resized_rightMid_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
+  rightMidPredicted.src = "/templates/static/images/predicted/resized_rightMid_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
+
+  //----- 하단 -----
+  //좌하단
+  leftBotResized.src = "/templates/static/images/resized/resized_leftBot_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
+  leftBotPredicted.src = "/templates/static/images/predicted/resized_leftBot_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
+  //중하단
+  midBotResized.src = "/templates/static/images/resized/resized_midBot_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
+  midBotPredicted.src = "/templates/static/images/predicted/resized_midBot_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
+  //우하단
+  rightBotResized.src = "/templates/static/images/resized/resized_rightBot_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
+  rightBotPredicted.src = "/templates/static/images/predicted/resized_rightBot_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
+}
+
 var topSlider = document.getElementById("topImgSlider");
 var topOutput = document.getElementById("topValue");
 topOutput.innerHTML = topSlider.value; // Display the default slider value
