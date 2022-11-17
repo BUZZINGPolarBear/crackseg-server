74bfdf61ac0bf6f3a42d20bdfa79d04ef486a3b3
Jeonjunhowi <jjh63360@kau.kr>
2022-11-17 21:40:52 +0900

    fix: resized img를 이미지 형식 상관 없이 보여주기

--- templates/static/js/index-detailed-third.js
+++ templates/static/js/index-detailed-third.js
@@ -7,35 +7,35 @@
 
 window.onload = function(){
   //좌상단
-  leftTopResized.src = "/templates/static/images/resized/resized_leftTop_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
+  leftTopResized.src = "/templates/static/images/resized/resized_leftTop_"+ localStorage.getItem("pic_name")
   leftTopPredicted.src = "/templates/static/images/predicted/resized_leftTop_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
   //중상단
-  midTopResized.src = "/templates/static/images/resized/resized_midTop_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
+  midTopResized.src = "/templates/static/images/resized/resized_midTop_"+ localStorage.getItem("pic_name")
   midTopPredicted.src = "/templates/static/images/predicted/resized_midTop_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
   //우상단
-  rightTopResized.src = "/templates/static/images/resized/resized_rightTop_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
+  rightTopResized.src = "/templates/static/images/resized/resized_rightTop_"+ localStorage.getItem("pic_name")
   rightTopPredicted.src = "/templates/static/images/predicted/resized_rightTop_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
 
   //----- 중간 -----
   //좌중단
-  leftMidResized.src = "/templates/static/images/resized/resized_leftMid_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
+  leftMidResized.src = "/templates/static/images/resized/resized_leftMid_"+ localStorage.getItem("pic_name")
   leftMidPredicted.src = "/templates/static/images/predicted/resized_leftMid_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
   //중중단
-  midMidResized.src = "/templates/static/images/resized/resized_midMid_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
+  midMidResized.src = "/templates/static/images/resized/resized_midMid_"+ localStorage.getItem("pic_name")
   midMidPredicted.src = "/templates/static/images/predicted/resized_midMid_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
   //우중단
-  rightMidResized.src = "/templates/static/images/resized/resized_rightMid_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
+  rightMidResized.src = "/templates/static/images/resized/resized_rightMid_"+ localStorage.getItem("pic_name")
   rightMidPredicted.src = "/templates/static/images/predicted/resized_rightMid_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
 
   //----- 하단 -----
   //좌하단
-  leftBotResized.src = "/templates/static/images/resized/resized_leftBot_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
+  leftBotResized.src = "/templates/static/images/resized/resized_leftBot_"+ localStorage.getItem("pic_name")
   leftBotPredicted.src = "/templates/static/images/predicted/resized_leftBot_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
   //중하단
-  midBotResized.src = "/templates/static/images/resized/resized_midBot_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
+  midBotResized.src = "/templates/static/images/resized/resized_midBot_"+ localStorage.getItem("pic_name")
   midBotPredicted.src = "/templates/static/images/predicted/resized_midBot_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
   //우하단
-  rightBotResized.src = "/templates/static/images/resized/resized_rightBot_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpeg"
+  rightBotResized.src = "/templates/static/images/resized/resized_rightBot_"+ localStorage.getItem("pic_name")
   rightBotPredicted.src = "/templates/static/images/predicted/resized_rightBot_"+ localStorage.getItem("pic_name").split(".")[0] + ".jpg"
 }
 
