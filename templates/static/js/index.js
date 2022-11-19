65cd3abad4092504fc2aa83e608c82ce8524fc47
Jeonjunhowi <jjh63360@kau.kr>
2022-11-19 14:35:03 +0900

    add: 정밀 균열 검출 모달창 추가

--- templates/static/js/index.js
+++ templates/static/js/index.js
@@ -1,6 +1,5 @@
 const crackImgModalBtn = document.getElementById("crack-img-submit-btn");
-const crackImgSubmitBtn = document.getElementById("normal-crack-btn")
-const detailedCrackImgSubmitBtn = document.getElementById("detailed-crack-img-submit-btn")
+const detailedImgModalBtn = document.getElementById("detailed-crack-img-submit-btn");
 var fileName = $("#imgInp").val();
 
 imgInp.onchange = evt => {
@@ -20,57 +19,7 @@ function toLoadingPage(){
   location.href="loading/"
 }
 
-crackImgSubmitBtn.onclick = async function(){
-  if(fileName.length==0){
-    alert("사진 파일을 입력해주세요.")
-  }
-  else{
-    var formdata = new FormData();
-    const file = imgInp.files[0];
-    const pic_rand_id = makeid(15);
 
-    formdata.append("title", pic_rand_id);
-    formdata.append("imgfile", file, fileName);
-
-    var requestOptions = {
-      method: 'POST',
-      body: formdata,
-      redirect: 'follow'
-    };
-    modal.style.display = "none"
-    toLoadingPage()
-    localStorage.setItem("pic_name", pic_rand_id+'.jpg')
-    await getAPI(hostAddr + "crack-seg/remove-imgs")
-    await postAPI(hostAddr+"crack-seg/fileupload/", requestOptions)
-    location.href = "result/"
-  }
-}
-
-detailedCrackImgSubmitBtn.onclick = async function(){
-  if(fileName.length==0){
-    alert("사진 파일을 입력해주세요.")
-  }
-  else{
-    var formdata = new FormData();
-    const file = imgInp.files[0];
-    const pic_rand_id = makeid(15);
-
-    formdata.append("title", pic_rand_id);
-    formdata.append("imgfile", file, fileName);
-
-    var requestOptions = {
-      method: 'POST',
-      body: formdata,
-      redirect: 'follow'
-    };
-    modal.style.display = "none"
-    toLoadingPage()
-    localStorage.setItem("pic_name", pic_rand_id+'.jpg')
-    await getAPI(hostAddr + "crack-seg/remove-imgs")
-    await postAPI(hostAddr+"crack-seg/fileuplaod/detailed", requestOptions)
-    location.href = "result/detailed"
-  }
-}
 
 //post API AS JSON
 async function postAPI(host, options) {
@@ -98,7 +47,71 @@ function makeid(length) {
 
 //modals
 crackImgModalBtn.onclick = function(){
-   modal.style.display = "flex"
+  $('#modal-btn-area').append(`
+    <button class="button is-primary" id="normal-crack-btn">일반 균열 검출하기</button>
+  `)
+   modal.style.display = "flex";
+
+  const crackImgSubmitBtn = document.getElementById("normal-crack-btn");
+  crackImgSubmitBtn.onclick = async function(){
+  if(fileName.length==0){
+    alert("사진 파일을 입력해주세요.")
+  }
+  else{
+    var formdata = new FormData();
+      const file = imgInp.files[0];
+      const pic_rand_id = makeid(15);
+
+      formdata.append("title", pic_rand_id);
+      formdata.append("imgfile", file, fileName);
+
+      var requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+      };
+      modal.style.display = "none"
+      toLoadingPage()
+      localStorage.setItem("pic_name", pic_rand_id+'.jpg')
+      await getAPI(hostAddr + "crack-seg/remove-imgs")
+      await postAPI(hostAddr+"crack-seg/fileupload/", requestOptions)
+      location.href = "result/"
+    }
+  }
+}
+
+detailedImgModalBtn.onclick = function(){
+  $('#modal-btn-area').append(`
+   <button class="button is-warning modal-btn" id="detailed-crack-btn">정밀 균열 검출하기</button>
+  `)
+   modal.style.display = "flex";
+
+  const detailedCrackImgSubmitBtn = document.getElementById("detailed-crack-btn")
+  detailedCrackImgSubmitBtn.onclick = async function(){
+  if(fileName.length==0){
+    alert("사진 파일을 입력해주세요.")
+  }
+  else{
+      var formdata = new FormData();
+      const file = imgInp.files[0];
+      const pic_rand_id = makeid(15);
+
+      formdata.append("title", pic_rand_id);
+      formdata.append("imgfile", file, fileName);
+
+      var requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+      };
+      modal.style.display = "none"
+      toLoadingPage()
+      localStorage.setItem("pic_name", pic_rand_id+'.jpg')
+      await getAPI(hostAddr + "crack-seg/remove-imgs")
+      await postAPI(hostAddr+"crack-seg/fileuplaod/detailed", requestOptions)
+      location.href = "result/detailed"
+    }
+  }
 }
 const closeBtn = modal.querySelector(".close-area")
 closeBtn.addEventListener("click", e => {
